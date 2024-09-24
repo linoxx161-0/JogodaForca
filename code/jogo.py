4c44a5f09336dfe6a115b35f4901ba76b389f186
Maynara Lino Amaral <108151583+linoxx161-0@users.noreply.github.com>
2024-09-24 19:12:17 +0000

    Adicionando o código ao repositório

---
+++ code/jogo.py
@@ -0,0 +1,53 @@
+import random
+
+#Criando a função do jogo
+def game():
+    print("\n Bem vindo ao Jogo da Forca!")
+    print("Advinhe a palavra abaixo:\n")
+
+    #Criando uma lista com as palavras do jogo
+    palavras_possiveis = ["letra", "exibição", "lista", "espaço"]
+
+    #Atribuindo uma palavra aleatória da lista de palavras possíveis à variável
+    palavra = random.choice(palavras_possiveis)
+
+    #List Comprehension atribuindo um underline para cada letra na variável palavra
+    letras_adivinhadas = ["_" for letra in palavra]
+
+    #Criar uma lista vazia para armazenar as letras erradas:
+    letras_erradas = []
+
+    #Definir o número máximo de chances:
+    chances = 8
+
+#Pedir ao jogador que advinhe uma letra
+letra = input("Digite uma letra: ")
+
+#Verificar se a letra adivinhada está na palavra:
+for letra in palavra:
+    while chances > 0:
+    print(" ".join(letras_adivinhadas))
+    print("\nChances restantes:", chances)
+    print("Letras erradas:", " ".join(letras_erradas))
+
+    #Tentativa
+    tentativa = input("\nDigite uma letra: ").lower()
+
+    #Condicional
+    if tentativa in palavra:
+        index = 0
+
+        for letra in palavra:
+            if tentativa == letra:
+                letras_adivinhadas[index] = letra
+            index +=1
+        else:
+            chances -= 1
+            letras_erradas.append(tentativa)
+
+        if "_" not in letras_adivinhadas:
+            print("\nVocê venceu! A palavra era:", palavra)
+            break
+    if "_" in letras_adivinhadas:
+        print("\nVocê perdeu! A palavra era: ", palavra )
+
